976845d854fe0d8b68df11371c9651c12fd42c30
Soyoung Lee <soo7132@naver.com>
2025-01-10 10:45:55 +0900

    part1-이소영-sprint1: ServiceFactory 주석 정리

--- src/main/java/discodeit/service/ServiceFactory.java
+++ src/main/java/discodeit/service/ServiceFactory.java
@@ -6,16 +6,10 @@ import discodeit.service.jcf.JCFUserService;
 
 public class ServiceFactory {
 
-    private static ServiceFactory instance;
     private final JCFUserService jcfUserService;
     private final JCFChannelService jcfchannelService;
     private final JCFMessageService jcfMessageService;
 
-//    private ServiceFactory() {
-//        jcfUserService = JCFUserService.getInstance();
-//        jcfchannelService = JCFChannelService.getInstance();
-//        jcfMessageService = JCFMessageService.getInstance();
-//    }
 
     public ServiceFactory() {
         jcfUserService = JCFUserService.getInstance();
@@ -23,16 +17,6 @@ public class ServiceFactory {
         jcfMessageService = JCFMessageService.getInstance();
     }
 
-//    public static ServiceFactory getInstance() {
-//        if(instance == null) {
-//            synchronized (ServiceFactory.class) {
-//                if(instance == null) {
-//                    instance = new ServiceFactory();
-//                }
-//            }
-//        }
-//        return instance;
-//    }
 
     //Getter
     public JCFUserService getJcfUserService() {
