91d11de10bc418213b0c60d65b3d5b9aea94d144
Soyoung Lee <soo7132@naver.com>
2025-05-21 13:19:30 +0900

    sprint 9: 기존 auth login 로직 삭제

--- src/main/java/com/spirnt/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/spirnt/mission/discodeit/controller/AuthController.java
@@ -1,17 +1,12 @@
 package com.spirnt.mission.discodeit.controller;
 
 import com.spirnt.mission.discodeit.controller.swagger.AuthApiDocs;
-import com.spirnt.mission.discodeit.dto.auth.LoginRequest;
-import com.spirnt.mission.discodeit.dto.user.UserDto;
 import com.spirnt.mission.discodeit.service.AuthService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,14 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController implements AuthApiDocs {
 
     private final AuthService authService;
-
-    @PostMapping("/login")
-    public ResponseEntity<UserDto> login(@Valid @RequestBody LoginRequest loginRequest) {
-        log.info("[Login Request Started]");
-        UserDto user = authService.login(loginRequest);
-        log.info("[Login Request Completed]");
-        return ResponseEntity.ok(user);
-    }
 
     @GetMapping("/csrf-token")
     public ResponseEntity<CsrfToken> getCsrfToken(CsrfToken csrfToken) {
--- src/main/java/com/spirnt/mission/discodeit/service/AuthService.java
+++ src/main/java/com/spirnt/mission/discodeit/service/AuthService.java
@@ -1,9 +1,5 @@
 package com.spirnt.mission.discodeit.service;
 
-import com.spirnt.mission.discodeit.dto.auth.LoginRequest;
-import com.spirnt.mission.discodeit.dto.user.UserDto;
-
 public interface AuthService {
 
-  UserDto login(LoginRequest loginRequest);
 }
