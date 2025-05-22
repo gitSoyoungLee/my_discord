0047de1efe348ec4a9d8e584b719ca68cc4c1275
Soyoung Lee <soo7132@naver.com>
2025-05-22 15:11:03 +0900

    sprint 9 심화: 동시 세션 제어

--- src/main/java/com/spirnt/mission/discodeit/config/SecurityConfig.java
+++ src/main/java/com/spirnt/mission/discodeit/config/SecurityConfig.java
@@ -60,6 +60,8 @@ public class SecurityConfig {
                     "/api/auth/logout"))
             // 커스텀 인증 필터 추가: UserAuthenticationFilter 대신 CustomAuthenticationFilter 사용
             .addFilterAt(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+            .sessionManagement(s -> s.maximumSessions(1)    // 동시 세션 제어: 동시 세션 최대 1개 허용
+                .maxSessionsPreventsLogin(false))   // 새로운 로그인 시 기존 세션을 무효화
             .logout(logout -> logout.logoutRequestMatcher(
                     new AntPathRequestMatcher("/api/auth/logout"))  // POST /api/auth/logout으로 로그아웃
                 .logoutSuccessUrl("/") // 세션 무효화 후 홈으로
