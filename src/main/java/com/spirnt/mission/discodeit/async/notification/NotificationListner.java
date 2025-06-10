ae7dd4a4011aeeb0fb4519d4f459cfd3b0e87882
Soyoung Lee <soo7132@naver.com>
2025-06-10 18:15:23 +0900

    sprint 11: Kafka 도입 - Kafka Consumer 기반

--- src/main/java/com/spirnt/mission/discodeit/async/notification/NotificationListner.java
+++ src/main/java/com/spirnt/mission/discodeit/async/notification/NotificationListner.java
@@ -3,10 +3,7 @@ package com.spirnt.mission.discodeit.async.notification;
 import com.spirnt.mission.discodeit.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.event.TransactionPhase;
-import org.springframework.transaction.event.TransactionalEventListener;
 
 /**
  * 비동기 방식 알림 생성 리스너
@@ -18,10 +15,10 @@ public class NotificationListner {
 
     private final NotificationService notificationService;
 
-    @Async("propagatingExecutor")
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    public void createNotification(NotificationCreateEvent notificationCreateEvent) {
-        notificationService.create(notificationCreateEvent);
-    }
+//    @Async("propagatingExecutor")
+//    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+//    public void createNotification(NotificationCreateEvent notificationCreateEvent) {
+//        notificationService.create(notificationCreateEvent);
+//    }
 
 }
